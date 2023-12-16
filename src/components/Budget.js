e789db32b6bfb7be2095a71de5a8ae6cfc185158
Harrison <bestnottoemailme@gmail.com>
2023-12-16 14:28:54 -0500

    add functions

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -2,19 +2,33 @@ import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
 
 const Budget = () => {
-    const { budget, dispatch } = useContext(AppContext);
+    const { budget, dispatch, expenses, currency} = useContext(AppContext);
     const [newBudget, setNewBudget] = useState(budget);
     const handleBudgetChange = (event) => {
+        const totalExpenses = expenses.reduce((total, item) => {
+            return (total = total + item.cost);
+        }, 0);
+        if (event.target.value > 20000) {
+            window.alert(
+                "Cannot exceed 20000!"
+            );
+            return;
+        } else if (event.target.value < totalExpenses) {
+            window.alert(
+                "Cannot lower than spending!"
+            );
+            return;
+        }
         setNewBudget(event.target.value);
-        dispatch({
-            type: 'SET_BUDGET',
-            payload: newBudget,
-        });
+        // dispatch({
+        //     type: 'SET_BUDGET',
+        //     payload: newBudget,
+        // });
     }
     return (
 <div className='alert alert-secondary'>
-<span>Budget: £{budget}</span>
-<input type="number" step="10" value={newBudget} onChange={handleBudgetChange}></input>
+<span>Budget: {currency}{budget}</span>
+<input type="number" step="10" value={newBudget} max={20000} onChange={handleBudgetChange}></input>
 </div>
     );
 };
